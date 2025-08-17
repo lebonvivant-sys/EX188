1c2e2a3f4d98826cf419c268097ba19f78c6b3c0
Lebon Vivant <lebonvivant@hotmail.nl>
2025-08-17 13:49:01 +0200

    console php-helloworld

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,7 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming:\n";
 print "Edit in place!\n";
+print "Edit in place!\n";
+print "Edit in place!\n";
+print "php console Edit in place!\n";
 ?>
